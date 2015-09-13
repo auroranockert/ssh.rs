ceafca2789a518e17dc02e711c2460d7212367ef
Jens Nockert <jens@nockert.se>
2015-09-13 13:56:20 +0200

    Donate butan

--- src/ssh.rs
+++ src/ssh.rs
@@ -13,6 +13,12 @@
 //! Warning: Everything is quite slow.
 //!
 //! ![sshlogo](http://neophob.com/wp-content/uploads/2010/05/sshlogo.png)
+//! <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+//! <input type="hidden" name="cmd" value="_s-xclick">
+//! <input type="hidden" name="hosted_button_id" value="4ARJP4UBNMDW2">
+//! <input type="image" src="https://www.paypalobjects.com/en_US/SE/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+//! <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+//! </form>
 
 extern crate num;
 extern crate rand;
