e6fad9cef30e35510d23e47d1473eed61c560616
Jens Nockert <jens@nockert.se>
2015-09-15 18:05:45 +0200

    Refactor socket

--- src/transport/ssh_socket.rs
+++ src/transport/ssh_socket.rs
@@ -10,30 +10,53 @@ pub struct Socket<'a> {
   writer: &'a mut io::Write
 }
 
-static SSH_IDENTIFIER_CLIENT: &'static str = "SSH-2.0-ssh.rs_0.0.1";
+static SSH_IDENTIFIER: &'static str = "SSH-2.0-ssh.rs_0.0.1";
 
 impl<'a> Socket<'a> {
   pub fn new(reader: &'a mut io::Read, writer: &'a mut io::Write) -> Socket<'a> {
     return Socket { reader: reader, writer: writer };
   }
 
-  pub fn version_exchange(&mut self) -> VersionExchange {
-    let mut client_identifier = SSH_IDENTIFIER_CLIENT.to_owned();
+  pub fn version_exchange(&mut self, server: bool) -> VersionExchange {
+    let mut identifier = SSH_IDENTIFIER.to_owned();
 
-    client_identifier.push('\r');
-    client_identifier.push('\n');
-    
-    assert_eq!(self.writer.write(client_identifier.as_bytes()).unwrap(), client_identifier.len());
+    identifier.push('\r');
+    identifier.push('\n');
 
+    assert_eq!(self.writer.write(identifier.as_bytes()).unwrap(), identifier.len());
+
+    if server {
+      return VersionExchange {
+        client: self.read_version(),
+        server: SSH_IDENTIFIER.to_owned()
+      };
+    } else {
+      return VersionExchange {
+        client: SSH_IDENTIFIER.to_owned(),
+        server: self.read_server_version()
+      };
+    }
+  }
+
+  fn read_version(&mut self) -> String {
     let mut name = read_until(self.reader, '\n');
-    assert_eq!(name.pop(), Some('\r'));
 
+    assert_eq!(name.pop(), Some('\r'));
     assert_eq!(&name[0 .. 8], "SSH-2.0-");
 
-    return VersionExchange {
-      client: SSH_IDENTIFIER_CLIENT.to_owned(),
-      server: name
-    };
+    return name;
+  }
+
+  fn read_server_version(&mut self) -> String {
+    let mut name = read_until(self.reader, '\n');
+
+    if name.pop() != Some('\r') || &name[0 .. 4] != "SSH-" {
+      return self.read_server_version();
+    }
+
+    assert_eq!(&name[4 .. 8], "2.0-");
+
+    return name;
   }
 }
 
@@ -80,7 +103,7 @@ mod tests {
 
   use super::Socket;
 
-  use super::SSH_IDENTIFIER_CLIENT;
+  use super::SSH_IDENTIFIER;
 
   static SSH_IDENTIFIER_SERVER: &'static str = "SSH-2.0-OpenSSH_6.2";
   static OLD_IDENTIFIER_SERVER: &'static str = "SSH-1.0-OpenSSH_6.2";
@@ -98,16 +121,16 @@ mod tests {
     let version_exchange = {
       let mut socket = Socket::new(&mut reader, &mut writer);
 
-      socket.version_exchange()
+      socket.version_exchange(false)
     };
 
-    let mut client_identifier = SSH_IDENTIFIER_CLIENT.as_bytes().to_owned();
+    let mut client_identifier = SSH_IDENTIFIER.as_bytes().to_owned();
 
     client_identifier.push('\r' as u8);
     client_identifier.push('\n' as u8);
 
     assert_eq!(writer.into_inner(), client_identifier);
-    assert_eq!(version_exchange.client, SSH_IDENTIFIER_CLIENT);
+    assert_eq!(version_exchange.client, SSH_IDENTIFIER);
     assert_eq!(version_exchange.server, SSH_IDENTIFIER_SERVER);
   }
 
@@ -124,6 +147,6 @@ mod tests {
     
     let mut socket = Socket::new(&mut reader, &mut writer);
 
-    let _ = socket.version_exchange();
+    let _ = socket.version_exchange(false);
   }
 }
